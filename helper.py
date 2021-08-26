ab2fb27838b3a96ac9fe0231bece3d381ad93b19
Gygarte <gabriel.artemie@gmail.com>
2021-08-26 23:25:06 +0300

    55

--- helper.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-model = {'BEAR':{'ASK':[],"BID":[]},
-         'BULL':{'ASK':[],'BID':[]},
-         'RETC':{'ASK':[],'BID':[]},
-         'USD':{'ASK':[], 'BID':[]}}
-
-def price_view_helper():
-    df = pd.DataFrame
